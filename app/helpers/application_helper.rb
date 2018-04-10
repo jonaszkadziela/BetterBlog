2576ee7e47dc51d7359a3745f7f30e13dbd873cc
jonaszkadziela <jonaszkadziela@o2.pl>
2018-04-11 00:38:48 +0200

    Created user-friendly UI & did a lot of styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level.to_sym
+    when :notice then "alert my-alert-info"
+    when :success then "alert my-alert-info"
+    when :error then "alert alert-error"
+    when :alert then "alert alert-error"
+    end
+  end
 end
