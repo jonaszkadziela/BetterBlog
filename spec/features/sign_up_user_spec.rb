3f483bef13c8ec9f0e7a52838d53f4ed5ace78b1
jonaszkadziela <jonaszkadziela@o2.pl>
2018-04-13 14:20:00 +0200

    Add sign up user spec

--- spec/features/sign_up_user_spec.rb
+++ spec/features/sign_up_user_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.feature "A user signs up" do
   let(:user) { FactoryBot.build(:user) }
 
-  scenario "" do
+  scenario "with valid credentials" do
     visit "/"
 
     click_link "Sign in"
@@ -16,5 +16,22 @@ RSpec.feature "A user signs up" do
     click_button "Sign up"
 
     expect(page).to have_content("Welcome! You have signed up successfully.")
+  end
+
+  scenario "with invalid credentials" do
+    visit "/"
+
+    click_link "Sign in"
+    click_link "Sign up"
+
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+
+    click_button "Sign up"
+    
+    expect(page).to have_content("prohibited this user from being saved")
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content("Password can't be blank")
   end
 end
