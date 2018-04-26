cc04d11bfe40e39eeca389e3857ba4f29bba9ce8
jonaszkadziela <jonaszkadziela@o2.pl>
2018-04-26 22:10:09 +0200

    Add markdown parser & styles

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'autoprefixer-rails', '~> 8.2'
 gem 'devise', '~> 4.4', '>= 4.4.3'
 gem 'will_paginate', '~> 3.1', '>= 3.1.6'
+gem 'redcarpet', '~> 3.4'
+gem 'pygments.rb', '~> 1.2', '>= 1.2.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,28 @@ module ApplicationHelper
     when :alert then "alert my-alert-danger"
     end
   end
+
+  def markdown(content)
+    options = {
+      hard_wrap: true,
+      filter_html: true,
+      tables: true,
+      autolink: true,
+      no_intra_emphasis: true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true
+    }
+
+    renderer = HTMLwithPygments.new()
+    Redcarpet::Markdown.new(renderer, options).render(content).html_safe
+  end
+
+  class HTMLwithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
 end
